3d60bbd80e9a4884f9099c13abc6436a1664f664
min <hello.git.minwoo867412@naver.com>
2023-09-05 08:31:47 +0900

    전체적인 사이트 구축및 로그인 기능구현

---
+++ BBS/src/main/java/user/User.java
@@ -0,0 +1,43 @@
+package user;
+
+public class User {
+
+	private String USERID;
+	private String userPassword;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	public String getUSERID() {
+		return USERID;
+	}
+	public void setUSERID(String uSERID) {
+		USERID = uSERID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
+	
+	
+	
